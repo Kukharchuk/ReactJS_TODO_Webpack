0e0ac7e5079dc45057214e58935283fc372ba350
Maryia Kukharchuk <m.kukharchuk@godeltech.com>
2018-01-29 14:38:08 +0300

    css-loader&style-loader were added

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,11 @@ module.exports = {
             test: /\.(js|jsx)$/,
             exclude: /node_modules/,
             use: ['babel-loader']
-          }
+          },
+          {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        }
         ]
       },
       resolve: {
